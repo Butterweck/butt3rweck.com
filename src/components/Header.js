240f77ba3cb712f71b9f07f53a0f1291876b27ba
Sebastian Butterweck <sebastian@butt3rweck.com>
2019-01-02 16:53:23 +0100

    img scales again, simplified div structure in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,16 @@ export class Header extends Component {
   render() {
     return (
       <div class="Block-1 Block-1-Full Block-1-Type-Header">
-        <div class="Element-2-Type-Header-Image">
-          <img class="Subelem-3-Type-Header-Image" src={logo} alt="logo" />
-        </div>
-        <div class="Element-2-Type-Header-Text">
-          <p class="Subelem-3-Type-Header-Text">
+        
+          <img class="Element-2-Type-Header-Image" src={logo} alt="logo" />
+        
+        
+          <p class="Element-2-Type-Header-Text">
             Working with Scrum since almost 5 years, I am convinced that the
             success or failure of a dev team is highly depending on the degree
             of transparency that the team shows - both towards external and
             internal stakeholders.
-          </p>
-          <p class="Subelem-3-Type-Header-Text">
+          <br/>&nbsp;<br/>
             I supported dev teams from the fields of web, embedded and devops
             both as a product owner and scrum master. Also, I have a very good
             technical understanding and can communicate to developers on a
@@ -24,7 +23,7 @@ export class Header extends Component {
             is taking different perspectives, mediate and translate.
           </p>
         </div>
-      </div>
+      
     );
   }
 }
