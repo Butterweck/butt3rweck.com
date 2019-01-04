8b6ec5bd2ff376a35cc43b8da76c7170a87a9053
Sebastian Butterweck <sebastian@butt3rweck.com>
2019-01-05 00:51:46 +0100

    parameterized header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Projects } from "./components/Projects";
 import { Quotes } from "./components/Quotes";
 import { SimpleBlock } from "./components/SimpleBlock";
 import { Logobox } from "./components/Logobox";
+import profile from "./imgs/profile.jpg";
 
 class App extends Component {
   skills = {
@@ -29,18 +30,28 @@ class App extends Component {
     "info@butt3rweck.com": "FaAt"
   };
 
+  shortTexts = [
+    "Working with Scrum since almost 5 years, I am convinced that the success or failure of a dev team is highly depending on the degree of transparency that the team shows - both towards external and internal stakeholders.",
+    "I supported dev teams from the fields of web, embedded and devops both as a product owner and scrum master. Also, I have a very good technical understanding and can communicate to developers on a technical level. The most important thing I can support a team with is taking different perspectives, mediate and translate."
+  ];
+
   render() {
     return (
       <div class="Container">
         <Logobox />
         <div class="Outer-0">
           <SimpleBlock description="That's me" headline="true" />
-          <Header contactData={this.contactData} />
+          <Header
+            contactData={ this.contactData }
+            img={ profile }
+            alt="Sebastian Butterweck"
+            description={ this.shortTexts }
+          />
           <SimpleBlock
             description="That's what I offer to development teams."
             headline="true"
           />
-          <Field elements={this.skills} />
+          <Field elements={ this.skills } />
           <SimpleBlock />
           <SimpleBlock />
           <SimpleBlock />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,34 @@
 import React, { Component } from "react";
-import profile from "../imgs/profile.jpg";
 import "../App.css";
 import { SimpleBlock } from "./SimpleBlock";
 import { Field } from "./Field";
 
 export class Header extends Component {
   render() {
+    const descriptionLength = this.props.description.length;
+    var parsedDescription = this.props.description.map(function(text, index) {
+      if (descriptionLength == index) {
+        return text;
+      } else {
+        return (
+          <div>
+            {text} <br /> &nbsp; <br />
+          </div>
+        );
+      }
+    })
+
     return (
       <div>
-        
-        <Field elements = { this.props.contactData } />
+        <Field elements={this.props.contactData} />
         <div class="Block-1 Block-1-Full Block-1-Type-Header">
           <img
             class="Element-2-Type-Header-Image"
-            src={profile}
-            alt="profile"
+            src={this.props.img}
+            alt={this.props.alt}
           />
           <p class="Element-2-Type-Header-Text">
-            Working with Scrum since almost 5 years, I am convinced that the
-            success or failure of a dev team is highly depending on the degree
-            of transparency that the team shows - both towards external and
-            internal stakeholders.
-            <br />
-            &nbsp;
-            <br />I supported dev teams from the fields of web, embedded and
-            devops both as a product owner and scrum master. Also, I have a very
-            good technical understanding and can communicate to developers on a
-            technical level. The most important thing I can support a team with
-            is taking different perspectives, mediate and translate.
+            { parsedDescription }
           </p>
         </div>
       </div>
